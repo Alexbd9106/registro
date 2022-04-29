7a759c8302468d09cd0051a22b4033e01fbff915
Alexbd9106 <92698256+Alexbd9106@users.noreply.github.com>
2022-04-29 14:55:55 -0400

    Delete server directory

--- server/models/raza.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const razaSchema = mongoose.Schema({
-  nombre: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Raza', razaSchema);
